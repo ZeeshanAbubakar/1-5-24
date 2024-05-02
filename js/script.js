ba5b63be4e20a7fae0654bc74946f569d6f80ffc
ZeeshanAbubakar <malikzeeshi325@gmail.com>
2024-05-02 21:08:46 +0500

    asd

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// document.getElementById("h1").innerHTML = "<span>  this is span </span>"
+// document.getElementById("btn").onclick = function (){
+
+//     document.getElementById("h1").innerHTML = "<h6> learning JS </h6>"
+// }
+
+
+// var fullName = "Zeeshan"
+// document.write ("zeeshan")
+// // alert ('fullName')
+
+
+
+
+var firstName = "abdullah"
+var lastName = " anjum"
+
+
+var fullName = firstName + lastName
+function printMyName () {
+    console.log ('fullName')
+
+}
+printMyName ()
+
+
